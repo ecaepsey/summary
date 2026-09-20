94be1897e52a5ee3186a96839e5538c8f2b96dba
Damir Aushenov <yespeace@bk.ru>
2026-09-20 20:26:39 +0600

    Initial Commit

---
+++ summary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  summary
+//
+//  Created by Damir Aushenov on 20/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ summary/summaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  summaryApp.swift
+//  summary
+//
+//  Created by Damir Aushenov on 20/9/26.
+//
+
+import SwiftUI
+
+@main
+struct summaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
